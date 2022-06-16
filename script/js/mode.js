1bee4da7e297cf6e20cbc9758fdaa56cefb5c35e
DESKTOP-P76EL4N\steven <fallenyeh@gmail.com>
2022-06-16 13:10:30 +0800

    dark mode test

---
+++ script/js/mode.js
@@ -0,0 +1,11 @@
+$(".mode-select").click(function(){
+    if($(this).hasClass("active")){
+        $(this).removeClass("active");
+        $(this).find("i").removeClass("fa-sun").addClass("fa-moon");
+        $("body").removeClass("dark");
+    }else{
+        $(this).addClass("active");
+        $(this).find("i").removeClass("fa-moon").addClass("fa-sun");
+        $("body").addClass("dark");
+    }
+});
