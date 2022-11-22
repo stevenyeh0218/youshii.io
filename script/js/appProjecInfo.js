460502a49f81fc6843465905ecb4e7d1ba09bf74
DESKTOP-P76EL4N\steven <fallenyeh@gmail.com>
2022-11-22 22:23:40 +0800

    add  youdon app

--- script/js/appProjecInfo.js
+++ script/js/appProjecInfo.js
@@ -1,3 +1,8 @@
+/* YouDon */
+$(".appAproject_app_3").click(function () {
+    $(".appProject_info").addClass("active");
+    $(".appProject_info_app_3").addClass("imgShow");
+})
 
 /* 派車 */
 $(".appAproject_car").click(function () {
@@ -20,5 +25,5 @@ $(".appAproject_appointment").click(function () {
 /* 關閉視窗 */
 $(".appProject_info_closeBtn").click(function () {
     $(".appProject_info").removeClass("active");
-    $(".appProject_info_car , .appProject_info_dealer, .appProject_info_appointment").removeClass("imgShow");
+    $(".appProject_info_app_3,.appProject_info_car , .appProject_info_dealer, .appProject_info_appointment").removeClass("imgShow");
 })
