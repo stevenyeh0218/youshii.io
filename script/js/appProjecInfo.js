6727b39bc465c11f8d58c8b30ab54107fdce4c4f
DESKTOP-P76EL4N\steven <fallenyeh@gmail.com>
2022-11-22 22:08:01 +0800

    刪除多餘的程式

--- script/js/appProjecInfo.js
+++ script/js/appProjecInfo.js
@@ -1,35 +1,24 @@
-/* ML */
-$(".appAproject_app_1").click(function(){
-    $(".appProject_info").addClass("active");
-    $(".appProject_info_app_1").addClass("imgShow");
-})
-
-/* WEBPAT */
-$(".appAproject_app_2").click(function(){
-    $(".appProject_info").addClass("active");
-    $(".appProject_info_app_2").addClass("imgShow");
-})
 
 /* 派車 */
-$(".appAproject_car").click(function(){
+$(".appAproject_car").click(function () {
     $(".appProject_info").addClass("active");
     $(".appProject_info_car").addClass("imgShow");
 })
 
 /* 快易訂 */
-$(".appAproject_dealer").click(function(){
+$(".appAproject_dealer").click(function () {
     $(".appProject_info").addClass("active");
     $(".appProject_info_dealer").addClass("imgShow");
 })
 
 /* 預約 */
-$(".appAproject_appointment").click(function(){
+$(".appAproject_appointment").click(function () {
     $(".appProject_info").addClass("active");
     $(".appProject_info_appointment").addClass("imgShow");
 })
 
 /* 關閉視窗 */
-$(".appProject_info_closeBtn").click(function(){
+$(".appProject_info_closeBtn").click(function () {
     $(".appProject_info").removeClass("active");
-    $(".appProject_info_app_1 , .appProject_info_app_2 , .appProject_info_car , .appProject_info_dealer, .appProject_info_appointment").removeClass("imgShow");
+    $(".appProject_info_car , .appProject_info_dealer, .appProject_info_appointment").removeClass("imgShow");
 })
