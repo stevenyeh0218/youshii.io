4ed2e126336459e00d6b3cd67f67b4ecdf4992fe
DESKTOP-P76EL4N\steven <fallenyeh@gmail.com>
2023-02-16 20:43:14 +0800

    fixed cursor

--- js/cursor.js
+++ js/cursor.js
@@ -1,6 +1,6 @@
 /* cursor effect */
 const cursor = document.querySelector(".cursor");
-var btn = $(".menu_item , .mode-select , a , #topbtn");
+var btn = $(".menu_item , .mode-select , .sns > a , .colophon_inner_top_right_contact_item > a, #topbtn");
 var app = $(".appProjectItem , .project_link , .design_item");
 
 document.addEventListener("mousemove", (e) => {
