a64398782ce6dd31770ee1b467054e420b94c181
DESKTOP-P76EL4N\steven <fallenyeh@gmail.com>
2023-05-13 23:53:09 +0800

    優化

--- js/appProjecInfo.js
+++ js/appProjecInfo.js
@@ -1,29 +1,33 @@
-/* YouDon */
+// 函式: 顯示專案詳細資訊
+function showProjectInfo(targetClass) {
+	$(".appProject_info").addClass("active");
+	$(`.${targetClass}`).addClass("imgShow");
+	$('body').css('overflow', 'hidden');
+}
+
+// YouDon
 $(".appAproject_app_3").click(function () {
-    $(".appProject_info").addClass("active");
-    $(".appProject_info_app_3").addClass("imgShow");
-})
+	showProjectInfo("appProject_info_app_3");
+});
 
-/* 派車 */
+// 派車
 $(".appAproject_car").click(function () {
-    $(".appProject_info").addClass("active");
-    $(".appProject_info_car").addClass("imgShow");
-})
+	showProjectInfo("appProject_info_car");
+});
 
-/* 快易訂 */
+// 快易訂
 $(".appAproject_dealer").click(function () {
-    $(".appProject_info").addClass("active");
-    $(".appProject_info_dealer").addClass("imgShow");
-})
+	showProjectInfo("appProject_info_dealer");
+});
 
-/* 預約 */
+// 預約
 $(".appAproject_appointment").click(function () {
-    $(".appProject_info").addClass("active");
-    $(".appProject_info_appointment").addClass("imgShow");
-})
+	showProjectInfo("appProject_info_appointment");
+});
 
-/* 關閉視窗 */
+// 關閉視窗
 $(".appProject_info_closeBtn").click(function () {
-    $(".appProject_info").removeClass("active");
-    $(".appProject_info_app_3,.appProject_info_car , .appProject_info_dealer, .appProject_info_appointment").removeClass("imgShow");
-})
+	$(".appProject_info").removeClass("active");
+	$(".appProject_info_app_3,.appProject_info_car , .appProject_info_dealer, .appProject_info_appointment").removeClass("imgShow");
+	$('body').removeAttr("style");
+});
