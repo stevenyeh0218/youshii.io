5fc632688a4d8b229bb8a88297303543ebdae9a5
DESKTOP-P76EL4N\steven <fallenyeh@gmail.com>
2023-05-13 23:13:11 +0800

    優化

--- js/cursor.js
+++ js/cursor.js
@@ -4,25 +4,30 @@ var btn = $(".menu_item , .mode-select , .sns > a , .colophon_inner_top_right_co
 var app = $(".appProjectItem , .project_link , .design_item");
 
 document.addEventListener("mousemove", (e) => {
-    let x = e.pageX - 10;
-    let y = e.pageY - 5;
+	let x = e.pageX - 10;
+	let y = e.pageY - 5;
 
-    cursor.style.top = y + "px";
-    cursor.style.left = x + "px";
+	// 檢查游標是否接近視窗邊緣
+	if (x < 50 || x > window.innerWidth - 50) {
+		cursor.style.top = y + "px";
+	} else {
+		cursor.style.top = y + "px";
+		cursor.style.left = x + "px";
+	}
 });
 
 btn.on('mouseover', function () {
-    cursor.classList.add('is-hover');
+	cursor.classList.add('is-hover');
 });
 
 btn.on('mouseleave', function () {
-    cursor.classList.remove('is-hover');
+	cursor.classList.remove('is-hover');
 });
 
 app.on('mouseover', function () {
-    cursor.classList.add('is-project');
+	cursor.classList.add('is-project');
 });
 
 app.on('mouseleave', function () {
-    cursor.classList.remove('is-project');
+	cursor.classList.remove('is-project');
 });
