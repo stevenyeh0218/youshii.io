3eb3284feaec724d08144345fcfd2749a6bf9ed6
DESKTOP-P76EL4N\steven <fallenyeh@gmail.com>
2023-06-26 16:35:53 +0800

    fixed: 加入註解

--- js/scroll.js
+++ js/scroll.js
@@ -1,15 +1,18 @@
+/* Scroll Top */
 $(".top-btn").click(function () {
 	$("html,body").animate({
 		scrollTop: 0
 	}, "medium")
 });
 
+/* Scroll Next */
 $(".scroll-bar").click(function () {
 	$("html,body").animate({
 		scrollTop: $(".about").offset().top
 	}, "medium")
 });
 
+/* About Scroll Animate */
 $(window).on("scroll", function () {
 	var scrollPosition = $(this).scrollTop();
 	var aboutOffsetTop = $(".about").offset().top;
