ba6cf6dd101bb0d312d54eaaf4003196e8bca212
DESKTOP-P76EL4N\steven <fallenyeh@gmail.com>
2023-06-26 15:48:14 +0800

    feat: 加入about滑動動態效果

--- js/scroll.js
+++ js/scroll.js
@@ -9,3 +9,15 @@ $(".scroll-bar").click(function () {
 		scrollTop: $(".about").offset().top
 	}, "medium")
 });
+
+$(window).on("scroll", function () {
+	var scrollPosition = $(this).scrollTop();
+	var aboutOffsetTop = $(".about").offset().top;
+
+	if (scrollPosition < aboutOffsetTop) {
+		var percentage = (aboutOffsetTop - scrollPosition) / aboutOffsetTop * 100;
+		$(".about").css("clip-path", "inset(0% 0% 0% " + percentage.toFixed(4) + "%)");
+	} else {
+		$(".about").css("clip-path", "inset(0% 0% 0% 0%)");
+	}
+});
