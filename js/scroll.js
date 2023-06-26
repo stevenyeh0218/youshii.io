f35d8f10f6521e29ae9e4d722fa235bb361d0cd8
DESKTOP-P76EL4N\steven <fallenyeh@gmail.com>
2023-06-26 16:41:52 +0800

    feat: 移除 menu.js & 增加 menu anchor

--- js/scroll.js
+++ js/scroll.js
@@ -12,6 +12,20 @@ $(".scroll-bar").click(function () {
 	}, "medium")
 });
 
+/* Scroll To Home */
+$(".home-btn").click(function () {
+	$("html,body").animate({
+		scrollTop: $(".home").offset().top
+	}, "medium")
+});
+
+/* Scroll To About */
+$(".about-btn").click(function () {
+	$("html,body").animate({
+		scrollTop: $(".about").offset().top
+	}, "medium")
+});
+
 /* About Scroll Animate */
 $(window).on("scroll", function () {
 	var scrollPosition = $(this).scrollTop();
@@ -23,4 +37,25 @@ $(window).on("scroll", function () {
 	} else {
 		$(".about").css("clip-path", "inset(0% 0% 0% 0%)");
 	}
+});
+
+/* Scroll To Web */
+$(".web-btn").click(function () {
+	$("html,body").animate({
+		scrollTop: $(".web").offset().top
+	}, "medium")
+});
+
+/* Scroll To App */
+$(".app-btn").click(function () {
+	$("html,body").animate({
+		scrollTop: $(".app").offset().top
+	}, "medium")
+});
+
+/* Scroll To Contact */
+$(".contact-btn").click(function () {
+	$("html,body").animate({
+		scrollTop: $(".contact").offset().top
+	}, "medium")
 });
