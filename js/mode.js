6fbf8d3580a84cafcc7d4a6d6f7e84218baaec89
DESKTOP-P76EL4N\steven <fallenyeh@gmail.com>
2023-07-01 21:10:58 +0800

    feat: dark mode done

--- js/mode.js
+++ js/mode.js
@@ -1,11 +1,8 @@
-$(".mode-select").click(function () {
-    if ($(this).hasClass("active")) {
-        $(this).removeClass("active");
-        $(this).find("i").removeClass("fa-sun").addClass("fa-moon");
-        $("body").removeClass("dark");
-    } else {
-        $(this).addClass("active");
-        $(this).find("i").removeClass("fa-moon").addClass("fa-sun");
-        $("body").addClass("dark");
-    }
+$('.youshii-mode-switch').click(function () {
+	var isChecked = $('.youshii-mode-switch input').prop('checked');
+	if (isChecked) {
+		$('html').removeAttr('data-theme');
+	} else {
+		$('html').attr('data-theme', 'dark');
+	}
 });
