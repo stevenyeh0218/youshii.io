891442a96622ad887d55c61677349f6a0535ea9a
@Steven.Yeh <steven.yeh@mecotech.com.tw>
2023-07-15 21:58:57 +0800

    feat: 點擊後取消scroll-snap-align

--- js/scrollTop.js
+++ js/scrollTop.js
@@ -1,5 +1,8 @@
 $(".top-btn").click(function () {
-	$("html,body").animate({
-		scrollTop: 0
-	}, "medium")
+    $('.youshii-section').css("scroll-snap-align", "unset");
+    $("html,body").animate({
+        scrollTop: 0
+    }, "medium", function () {
+        $('.youshii-section').removeAttr('style');
+    });
 });
