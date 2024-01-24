cc5e141d002491dc516e486d25caab3326104afb
DESKTOP-P76EL4N\steven <fallenyeh@gmail.com>
2024-01-24 16:27:57 +0800

    fixed: web & app 架構調整

--- js/cursor.js
+++ js/cursor.js
@@ -2,22 +2,27 @@
 const cursor = document.querySelector(".cursor");
 var btn = $(".youshii-btn , nav > a , .youshii-mode-switch,.mobile-nav-switch");
 var scrollBtn = $(".top-btn,.scroll-bar");
-var web = $(".web > article > .item  > .title > a,.information > a");
-var app = $(".app > article > .item > .info > .title > a");
+var web = $(".web > article > .item ");
+var app = $(".app > article > .item ");
 
-
-document.addEventListener("mousemove", (e) => {
+function updateCursor(e) {
 	let x = e.pageX - 10;
 	let y = e.pageY - 5;
 
-	// 檢查游標是否接近視窗邊緣
 	if (x < 50 || x > window.innerWidth - 50) {
 		cursor.style.top = y + "px";
 	} else {
 		cursor.style.top = y + "px";
 		cursor.style.left = x + "px";
 	}
+}
+
+document.addEventListener("mousemove", (e) => {
+	requestAnimationFrame(() => {
+		updateCursor(e);
+	});
 });
+
 
 btn.on('mouseover', function () {
 	cursor.classList.add('is-hover');
